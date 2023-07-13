2e9ec15d08bcd97ce8d0f3e04198f6cb14b8dbf8
Romain DELCHIE <rdelchie@artem.local>
2023-07-13 15:17:36 +0200

    feat(navbar): navbar and burger menu done

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,27 +5,27 @@ import './NavBar.scss';
 export default function NavBar() {
     const [isBurgerOpen, setIsBurgerOpen] = useState(false);
     return (
-        <div className='navbar'>
+        <nav className='navbar'>
             <div className='navbar-logo'>
                 <Link to="/"><img className='navbar-logo' src="/images/logoCarre.jpg" alt="logo artem" /></Link>
             </div>
             <ul className={`navbar-links ${isBurgerOpen ? "show-navbar" : "hide-navbar"}`}>
-                <li>
+                <li className='navbar-item slideInDown1'>
                     <Link className='navbar-link' to="/">Nos produits</Link>
                 </li>
-                <li>
+                <li className='navbar-item slideInDown2'>
                     <Link className='navbar-link' to="/">Payer une facture</Link>
                 </li>
-                <li>
+                <li className='navbar-item slideInDown3'>
                     <Link className='navbar-link' to="/">Contact</Link>
                 </li>
-                <li>
+                <li className='navbar-item slideInDown4'>
                     <Link className='navbar-link' to="/test">test</Link>
                 </li>
             </ul>
             <button onClick={() => setIsBurgerOpen(!isBurgerOpen)} className='navbar-burger'>
                 <span className='burger-line'></span>
             </button>
-        </div >
+        </nav >
     )
 }
