467ce00c681fb80be529302c99f4120a53250779
Romain DELCHIE <rdelchie@artem.local>
2023-07-13 17:54:59 +0200

    feat(Home): add hero to home

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,13 +1,15 @@
 
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home/Home";
 import './App.scss'
 
 function App() {
 
 
   return (
-    <>
-      <div></div>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   )
 }
 
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import './Home.scss'
+
+export default function Home() {
+
+    return (
+        <main className='hero'>
+            <div className='hero-container'>
+                <h1 className='hero-title'>ARTEM</h1>
+                <h2 className='hero-subtitle'>Les supports d'avenir pour la pâte traditionnelle</h2>
+            </div>
+
+        </main>
+    )
+}
