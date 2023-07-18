7de1f18aa791a8ae9bd2504ea5a7a8a50bd1e1e0
Romain DELCHIE <rdelchie@artem.local>
2023-07-18 18:10:36 +0200

    style(home): change hero shadow

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -24,7 +24,7 @@ export default function Home() {
             </section>
 
             <section className='hero-description-container'>
-                <h3>Créer un compte afin d'accéder à plusieurs services :</h3>
+                <h3>Créer un compte et bénéficier ainsi de nos services :</h3>
                 <div className='hero-service-container'>
                     <div className='hero-service'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M14.25 7.756a4.5 4.5 0 100 8.488M7.5 10.5h5.25m-5.25 3h5.25M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
