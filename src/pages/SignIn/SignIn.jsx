9f8cce75fa43becb28a07662932df5dbb0e532e7
Romain DELCHIE <rdelchie@artem.local>
2023-07-24 12:30:57 +0200

    feat(signin): add page and style

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,9 +1,53 @@
+import React, { useState } from 'react';
 import './SignIn.scss'
 
 export default function SignIn() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    };
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Ici, vous pouvez mettre votre logique de connexion (envoyer les données au backend, vérifier les informations, etc.).
+        // Pour cet exemple, nous allons simplement afficher les données entrées dans la console.
+        console.log('Email:', Email);
+        console.log('Password:', password);
+    };
+
     return (
         <main className='signin'>
-            <h2>SignIn TODO</h2>
+            <h2>Connectez-vous et accéder à votre tableau de bord</h2>
+
+            <form className="signin-form" onSubmit={handleSubmit}>
+                <input
+                    className="signin-form-input"
+                    name="email"
+                    type="email"
+                    placeholder="Adresse email"
+                    value={email}
+                    onChange={handleEmailChange}
+                />
+                <input
+                    className="signin-form-input"
+                    name="password"
+                    type="password"
+                    placeholder="Mot de passe"
+                    value={password}
+                    onChange={handlePasswordChange}
+                />
+
+                <button className="signin-form-button" type="submit">
+                    Se connecter
+                </button>
+            </form>
         </main>
     )
 }
