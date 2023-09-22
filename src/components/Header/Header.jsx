b404fe230ee3e9b445f93f9ac8b95e4a6e060a3f
Romain-Delchie <roomsrio7782@gmail.com>
2023-09-22 15:37:12 +0200

    fix link with vitrine

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -51,7 +51,7 @@ export default function Header() {
 
             </div>
             {
-                location.pathname !== '/' && location.pathname !== '/dashboard' &&
+                location.pathname !== '/vitrine/' &&
                 <button onClick={handleGoBack} className='back-btn'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
