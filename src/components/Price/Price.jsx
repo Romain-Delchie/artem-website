52916a7be760085ed77642190bb55304532142df
Romain-Delchie <roomsrio7782@gmail.com>
2023-10-31 13:48:27 +0100

    fix goodprice error calcul and style tetool

--- src/components/Price/Price.jsx
+++ src/components/Price/Price.jsx
@@ -5,7 +5,7 @@ export default function Price({ product }) {
     const { user } = useContext(AppContext);
     product.price = product.t1 ? (product.t1 / 3) * 2 : product.price;
     const price = user.profile_id !== 2 ? (product.price * product.coeff).toFixed(2) : product.price.toFixed(2);
-    const goodPrice = price < product.minPrice ? product.minPrice.toFixed(2) : price;
+    const goodPrice = user.profile_id === 2 ? price : price < product.minPrice ? product.minPrice.toFixed(2) : price;
 
     return (
         <div className="price">
--- src/utils/goodPrice.js
+++ src/utils/goodPrice.js
@@ -2,7 +2,7 @@
 const goodPrice = (profil, product, quantity = 20) => {
     const coeffProd = product.reference.startsWith('TB') && quantity < 20 ? 1.1 : 1;
     const calculatedPrice = profil !== 2 ? (product.price * product.coeff) : product.price;
-    return calculatedPrice < product.minPrice ? (product.minPrice * coeffProd).toFixed(2) : (calculatedPrice * coeffProd).toFixed(2);
+    return profil === 2 ? calculatedPrice : calculatedPrice < product.minPrice ? (product.minPrice * coeffProd).toFixed(2) : (calculatedPrice * coeffProd).toFixed(2);
 }
 
 export default goodPrice;
