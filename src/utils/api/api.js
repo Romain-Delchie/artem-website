b1bdf3752d66160745333dfde21aac1cbcab771b
Romain-Delchie <roomsrio7782@gmail.com>
2023-11-06 15:29:54 +0100

    url api

--- src/utils/api/api.js
+++ src/utils/api/api.js
@@ -3,7 +3,7 @@ import Axios from "axios";
 let alertDisplayed = false;
 
 const axios = Axios.create({
-    baseURL: "http://localhost:3305/api",
+    baseURL: "http://85.215.34.177:3305/api",
     headers: {
         "Content-Type": "application/json",
     },
